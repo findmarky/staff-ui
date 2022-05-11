6a90dd81d3e6fc41cf9ae88e484c3b2d2c8f70d2
Mark Baker <mark.baker@xero.com>
2022-05-12 09:24:01 +1200

    Added more TODOs

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -3,19 +3,17 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 export const Navigation: FunctionComponent = () => {
   return (
-    <>
-      <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
-        <Container>
-          <Navbar.Brand href="#home">Application</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#users">Users</Nav.Link>
-              <Nav.Link href="#other">Other</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">Application</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#users">Users</Nav.Link>
+            <Nav.Link href="#other">Other</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
