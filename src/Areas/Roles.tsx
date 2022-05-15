063381f44d3af9031e948cd49c725274248044f0
Mark Baker <mark.baker@xero.com>
2022-05-16 08:25:55 +1200

    Populated the role description.

--- src/Areas/Roles.tsx
+++ src/Areas/Roles.tsx
@@ -1,16 +1,17 @@
 import { FunctionComponent } from "react";
 import { Button, Card, Container, Row, Col } from "react-bootstrap";
-import { chunk } from "lodash"
+import { chunk } from "lodash";
 import { v4 as uuidv4 } from "uuid";
 
 interface Role {
   id: String;
   name: String;
+  description: String;
 }
 
 type RoleCardPros = {
   role: Role;
-}
+};
 
 export const RoleCard: FunctionComponent<RoleCardPros> = ({ role }) => {
   const onCardClick = () => {
@@ -18,11 +19,11 @@ export const RoleCard: FunctionComponent<RoleCardPros> = ({ role }) => {
   };
 
   return (
-    <Card bg="Light" text={'dark'} onClick={onCardClick}>
+    <Card bg="Light" text={"dark"} onClick={onCardClick}>
       <Card.Header>{role.name}</Card.Header>
       <Card.Body>
         <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
+          {role.description}
         </Card.Text>
         <Button variant="primary">Something</Button>
       </Card.Body>
@@ -34,29 +35,30 @@ const NumberOfRolesInARow = 3;
 
 export const Roles: FunctionComponent = () => {
 
-  const roles = [
-    { name: 'Controller', id: "101" },
-    { name: 'Bookkeeper', id: "102" },
-    { name: 'Accountant', id: "103" },
-    { name: 'Staff Accountant', id: "104" },
-    { name: 'Accounting Clerk', id: "105" },
-    { name: 'Accounting Manager', id: "106" },
-    { name: 'Auditor', id: "107" },
-    { name: 'Tax Accountant', id: "108" }];
+  const roles: Role[] = [
+    { id: "101", name: "Controller", description: "In a corporate environment, a controller supervises all other accounting staff and usually reports to a chief financial officer or director of finance." },
+    { id: "102", name: "Bookkeeper", description: "The person in an organisation who is employed to perform bookkeeping functions is usually called the bookkeeper (or book-keeper)." },
+    { id: "103", name: "Accountant", description: "Accountants are professionals who record, summarize, evaluate and report financial transactions for organizations." },
+    { id: "104", name: "Staff Accountant", description: "A mid-level accounting position between junior accountant and senior accountant. At public accounting firms, staff accountant may be an entry-level position." },
+    { id: "105", name: "Accounting Clerk", description: "Accounting clerks are typically lower-level accounting professionals who are responsible for creating, reviewing and maintaining accounting records as well as keeping a record of business transactions." },
+    { id: "106", name: "Accounting Manager", description: "A mid to upper-level manager and accounting professional who oversees staff accountants and/or accounting supervisors or junior accountants. Generally manages a team of Accountants." },
+    { id: "107", name: "Auditor", description: "An auditor is responsible for analyzing an organization's reports and records, and they aid firms by ensuring public records are kept accurate and confirming the proper payment of taxes." },
+    { id: "108", name: "Tax Accountant", description: "A Tax Accountant is responsible for ensuring that businesses and individuals are in compliance with tax laws. Usually reporting financial tax information to external tax bodies." },
+  ];
 
-  const rowsOfRoles = chunk(roles, NumberOfRolesInARow);
+  const rowsOfRoles: Role[][] = chunk(roles, NumberOfRolesInARow);
 
   return (
     <Container>
-      {rowsOfRoles.map((rowOfRoles) => (
-        <Row key={uuidv4()} className="mt-4">
-          {rowOfRoles.map((role) => (
+      {rowsOfRoles.map((rowOfRoles: Role[], rowIndex: number) => (
+        <Row key={rowIndex} className="mt-4">
+          {rowOfRoles.map((role: Role) => (
             <Col key={uuidv4()} sm={4}>
-              <RoleCard key={role.id} role={role}></RoleCard>
+              <RoleCard key={role.id.toString()} role={role}></RoleCard>
             </Col>
           ))}
         </Row>
       ))}
-    </Container >
+    </Container>
   );
 };
