4b51c86888a039f489ce2ddab9a49b67f08d7ca5
Mark <findmarky@users.noreply.github.com>
2022-05-25 19:47:05 +1200

    Google Map

--- src/Components/UserCard.tsx
+++ src/Components/UserCard.tsx
@@ -1,7 +1,8 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, ReactElement, useEffect, useRef } from "react";
 import { Button, Card } from "react-bootstrap";
 import { UserInfo } from "../UserModel";
 import "./UserCard.css"
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 type UserCardProps = {
   user: UserInfo;
@@ -10,6 +11,40 @@ type UserCardProps = {
   onDelete: () => void;
   onEdit: () => void;
 };
+
+const render = (status: Status): ReactElement => {
+  if (status === Status.LOADING) return <h3>{status} ..</h3>;
+  if (status === Status.FAILURE) return <h3>{status} ...</h3>;
+  return <div></div>;
+};
+
+function MyMapComponent({ center, zoom, }: {
+  center: google.maps.LatLngLiteral;
+  zoom: number;
+}) {
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    new window.google.maps.Map(ref.current, {
+      center,
+      zoom,
+    });
+  });
+
+  return <div ref={ref} id="map" />;
+}
+
+function GMap() {
+  const center = { lat: -34.397, lng: 150.644 };
+  const zoom = 4;
+
+  return (
+    <Wrapper apiKey="" render={render}>
+      <MyMapComponent center={center} zoom={zoom} />
+    </Wrapper>
+  );
+}
+
 
 export const UserCard: FunctionComponent<UserCardProps> = ({
   user,
@@ -35,6 +70,11 @@ export const UserCard: FunctionComponent<UserCardProps> = ({
         <Button className="float-end" variant="danger" onClick={onDelete}>
           Delete
         </Button>
+
+        <GMap></GMap>
+
+
+
       </Card.Body>
     </Card>
   );
